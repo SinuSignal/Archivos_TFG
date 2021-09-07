9744f6f25f062586cf184918841d91f6dbae9076
SinuSignal <44363812+SinuSignal@users.noreply.github.com>
2021-09-07 22:49:21 +0200

    Delete getmacinfo.cmd

--- getmacinfo.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ----------- >>%1
-echo %date% %time% >>%1
-pause
-netsh wlan show interfaces | findstr /i /c:"Dirección" /c:"SSID" >> %1
-pause
-netsh interface ip show config name="Wi-fi" | findstr "Dirección IP" >>%1
-
-
